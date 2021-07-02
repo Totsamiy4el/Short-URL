271ba22c47966acbe12c20c695bc4d6cc7c62b3b
Totsamiy4el <saniok6652@gmail.com>
2021-07-02 22:26:53 +0300

    Uploading project

---
+++ Short-URL/Short-URL/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Short_URL
+{
+    class Program
+    {
+
+        static async void url()
+        {
+            var payload = new
+            {
+                destination = "https://docs.google.com/forms/d/1nrW3sdbhgWCFQwKF_VWiJqom7QFH89_eZTvqd0oSaCw/viewform?edit_requested=true",
+                domain = new
+                {
+                    fullName = "rebrand.ly"
+                }
+            };
+
+            using (var httpClient = new HttpClient { BaseAddress = new Uri("https://api.rebrandly.com") })
+            {
+                httpClient.DefaultRequestHeaders.Add("apikey", "269285c480d44721a2b25ec54f207d1a");
+                httpClient.DefaultRequestHeaders.Add("workspace", "5023336ecbed432fab19c77fa4a7fce9");
+
+                var body = new StringContent(
+                    JsonConvert.SerializeObject(payload), UnicodeEncoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("/v1/links", body))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var link = JsonConvert.DeserializeObject<dynamic>(
+                        await response.Content.ReadAsStringAsync());
+
+                    Console.WriteLine($"Long URL was {payload.destination}, short URL is {link.shortUrl}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            url();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Short-URL/Short-URL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Short-URL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Short-URL")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("03a33918-2d0b-425c-96de-5ce922c6b7a3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
